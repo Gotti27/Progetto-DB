91ca20f33047c25217ca93c8807860c399815d20
bitanp <nicola.bit00@gmail.com>
2021-05-02 12:39:47 +0200

    add "if __name__ =='__main__':" to run.py

--- run.py
+++ run.py
@@ -8,9 +8,8 @@ app.config.from_object("config")
 db = SQLAlchemy(app)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
 
-
-# FIXME: mettere if __name__ == "?" (non so che mettere al posto di ? ) ~Campa
 from appF.views import *
 # Importato non all'inizio per evitare dipendenze circolari
 
-app.run()
+if __name__ == '__main__':
+    app.run()
