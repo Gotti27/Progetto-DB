12a75a53e33d5c6d444a47b2f33810c82da9fef0
Alessio-Campa <878170@stud.unive.it>
2021-05-14 13:09:49 +0200

    Changes by bit

--- run.py
+++ run.py
@@ -6,11 +6,39 @@ from sqlalchemy import create_engine
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# TODO: CREARE CONNESSIONI DIVERSE
 db = SQLAlchemy(app)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
+"""
+engine_ospite = create_engine(app.config['SQLALCHEMY_DATABASE_URI_OSPITE'], echo=True)
+engine_utente = create_engine(app.config['SQLALCHEMY_DATABASE_URI_UTENTE'], echo=True)
+engine_istruttore = create_engine(app.config['SQLALCHEMY_DATABASE_URI_ISTRUTTORE'], echo=True)
+engine_gestore = create_engine(app.config['SQLALCHEMY_DATABASE_URI_GESTORE'], echo=True)
+"""
 
-from appF.views import *
-# Importato non all'inizio per evitare dipendenze circolari
+class User(UserMixin):
+    def __init__(self, id, email, pwd, role):
+        self.id = id
+        self.email = email
+        self.pwd = pwd
+        self.role = role
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+"""def load_user ( user_id ):
+        conn = engine.connect ()
+        rs = conn.execute ( ’ SELECT * FROM Users WHERE id = ? ’ ,user_id )
+        user = rs.fetchone ()
+        conn.close ()
+return User (user.id, user.email, user.pwd, user.role)"""
+
+from appF.views import * # Non importato all'inizio per evitare dipendenze circolari
 
 if __name__ == '__main__':
     app.run()
