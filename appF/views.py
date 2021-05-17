4c3f497935b34a21c8b8ad8a5c0637e1511548ed
Mario Gottardo <mario.gottardo27@gmail.com>
2021-05-17 17:02:41 +0200

    implementato hashing delle password

--- appF/views.py
+++ appF/views.py
@@ -1,6 +1,7 @@
 import re
 
 from datetime import *
+import bcrypt
 from flask import *
 from flask_login import login_user, logout_user, login_required
 
@@ -16,17 +17,14 @@ def home():
     return render_template("home.html")
 
 
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
-        password = request.form['password']
-        logging = db.session.query(Persona).filter(Persona.Email == username
-                                                   and Persona.Password == password).one()
-        if logging is not None:
+        password = bytes(request.form['password'], encoding="utf-8")
+        logging = db.session.query(Persona).filter(Persona.Email == username).first()
+        if logging is not None and bcrypt.checkpw(password, logging.Password.encode("utf-8")):
             user = get_persona_by_email(request.form['username'])
             login_user(user)
             return redirect(url_for('show_profile', username=user.Email))
@@ -55,25 +53,27 @@ def register():
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Indirizzo email non valido'
 
-        #elif not re.match(r'/^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T]
+        # elif not re.match(r'/^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T]
         # (?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T]
         # [26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L]
         # (?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i', CF):
         #    msg = 'Codice fiscale non valido'
-        #(http://blog.marketto.it/2016/01/regex-validazione-codice-fiscale-con-omocodia/) REGEX PER CODICE FISCALE
-        #TODO: testarlo
+        # (http://blog.marketto.it/2016/01/regex-validazione-codice-fiscale-con-omocodia/) REGEX PER CODICE FISCALE
+        # TODO: testarlo
 
         elif not request.form['name'] or not request.form['surname'] or not request.form['DataNascita'] or not \
-        request.form['Codice fiscale'] or not request.form['email'] or not request.form['sex'] or not request.form[
-            'password']:
+                request.form['Codice fiscale'] or not request.form['email'] or not request.form['sex'] or not \
+                request.form['password']:
             msg = 'Rimpire tutto il form'
         else:
             nuova_persona = insert_persona(nome=request.form['name'], cognome=request.form['surname'],
-                                 data_nascita=request.form['DataNascita'], cf=request.form['Codice fiscale'],
-                                 email=request.form['email'], telefono=request.form['telefono'],
-                                 sesso=request.form['sex'], password=request.form['password'], attivo=False)
+                                           data_nascita=request.form['DataNascita'], cf=request.form['Codice fiscale'],
+                                           email=request.form['email'], telefono=request.form['telefono'],
+                                           sesso=request.form['sex'],
+                                           password=bcrypt.hashpw(request.form['password'].encode("utf-8"),
+                                                                  bcrypt.gensalt()).decode("utf-8"), attivo=False)
             insert_cliente(nuova_persona)
-            # msg = 'Ti sei registrato con successo!' da rimuovere
+            # msg = 'Ti sei registrato con successo!' non visualizzato a causa del redirect
             login_user(nuova_persona)
             return redirect(url_for('show_profile', username=nuova_persona.Email))
     elif request.method == 'POST':
