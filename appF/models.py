062e3d955a3d82e949e71f970a532b996cbaa64b
Mario Gottardo <mario.gottardo27@gmail.com>
2021-07-02 09:39:06 +0200

    fix invia_notifica

--- appF/models.py
+++ appF/models.py
@@ -542,12 +542,11 @@ def crea_notifica(testo, mittente):
 
 def invia_notifica(notifica, destinatari):
     for person in destinatari:
-        if not session_istruttore.query(Persona).filter(Persona.CF == person).first():
-            return
-        to_add = NotificaDestinatario(IDNotifica=notifica.IDNotifica, Destinatario=person,
-                                      Timestamp=datetime.now(), Letto=False)
-        session_istruttore.add(to_add)
-        session_istruttore.commit()
+        if session_istruttore.query(Persona).filter(Persona.CF == person).first():
+            to_add = NotificaDestinatario(IDNotifica=notifica.IDNotifica, Destinatario=person,
+                                          Timestamp=datetime.now(), Letto=False)
+            session_istruttore.add(to_add)
+            session_istruttore.commit()
 
 
 def get_notifiche_persona(cf):
